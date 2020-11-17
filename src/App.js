104ba70a898fcd7e5f17e5c58f0e556e2c413a4e
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-17 12:28:31 -0600

    added functionality of retrieve token from the url and displayed the login component or something else based on whether there's a token or not

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React, { useEffect, useState } from "react";
 import Login from "./components/Login";
+import { getTokenFromResponse } from "./spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log(hash);
+  }, []);
+
+  return <div className="app">{token ? <h1>Logged in</h1> : <Login />}</div>;
 }
 
 export default App;
