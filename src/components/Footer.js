cb3a2b13bfc91be852d4e9cc75d9a83dbd3d2a12
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-19 12:05:45 -0600

    started importing icons for the footer and stylying them

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,45 @@
 import React from "react";
+import {
+  PlayCircleOutline,
+  PlaylistPlay,
+  Repeat,
+  Shuffle,
+  SkipNext,
+  SkipPrevious,
+  VolumeDown,
+} from "@material-ui/icons";
+import { Grid, Slider } from "@material-ui/core";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>Footer</h1>
+      <div className="footer__left">
+        <p>Album and song details</p>
+      </div>
+
+      <div className="footer__center">
+        <Shuffle className="footer__center-green" />
+        <SkipPrevious className="footer__center-icon" />
+        <PlayCircleOutline fontSize="large" className="footer__center-icon" />
+        <SkipNext className="footer__center-icon" />
+        <Repeat className="footer__center-icon" />
+      </div>
+
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlay />
+          </Grid>
+
+          <Grid item>
+            <VolumeDown />
+          </Grid>
+
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
