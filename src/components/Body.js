c91426d39df1048203b422e35be9662e5bb9f3d8
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2020-11-20 09:18:44 -0600

    created header component. Pulled profile image and name from the data layer and displayed it there

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
-function Body() {
+function Body({ spotify }) {
   return (
     <div className="body">
-      <h1>Body</h1>
+      <Header spotify={spotify} />
     </div>
   );
 }
